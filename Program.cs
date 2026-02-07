fe1e0b33caf3a0c36a3ebc7c226aa0f20d4863bc
zashkie <zashkiejakebontia@gmail.com>
2026-02-07 10:42:19 +0800

    Add basic calculator functionality in Program.cs
    Implemented a basic calculator that prompts the user for two numbers and an operation, allowing multiple calculations until the user decides to exit.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿//write your console display in here
+using System;
+
+public static class Calculator {
+    
+    //Main Method
+    public static void Main(String[] args){
+        bool continueCalculating = true;
+        
+        while (continueCalculating){
+            // Promts user to enter the first number
+            Console.Write("Enter first number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            
+            // Promts user to enter second number
+            Console.Write("Enter second number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            
+            string operation = "";
+            
+            do {
+                Console.WriteLine("Choose Operation (+,-,*,/,%,=)");
+                operation = Console.ReadLine();
+                
+                if (operation == "="){
+                    Calculate(first, second, operation);
+                    continueCalculating = false;
+                    break;
+                }
+                Calculate(first, second, operation);
+            }
+            while (operation != "+" && operation != "-" && operation != "*" && operation != "/" && operation != "%" && operation != "=");
+        }
+    }
+    
